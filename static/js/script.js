056e242fee37fd4a0f427cc2f4dba0a89388e62e
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-13 16:44:17 +0300

    staict folder with css and js

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+function initMap() {
+    // Create a new map instance
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 40.7128, lng: -74.0060 }, // Set the initial map center coordinates
+      zoom: 12, // Set the initial zoom level
+    });
+  }
+  
+  // Load the Google Maps API asynchronously
+  function loadScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap`;
+    document.head.appendChild(script);
+  }
+  
+  // Call the loadScript function to load the API
+  loadScript();
+  
